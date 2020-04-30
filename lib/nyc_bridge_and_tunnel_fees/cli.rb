303cc90217c7d8d4a2ede4084143c72a3c0ba5a9
dcantoran <davidcantoran36@gmail.com>
2020-04-30 18:18:52 -0400

    edited CLI

--- lib/nyc_bridge_and_tunnel_fees/cli.rb
+++ lib/nyc_bridge_and_tunnel_fees/cli.rb
@@ -1,6 +1,48 @@
 class NycBridgeAndTunnelFees::CLI 
     def call 
         puts "Welcome to NYC Bridges and Tunnels!"
-        binding.pry
+        puts ""
+        get_bridges
+        get_user_bridges
+          # get_prices
+    end
+
+    def get_bridges 
+      @bridges = ['The Bronx–Whitestone Bridge', 
+        'Throgs Neck Bridge',
+        'Tappan Zee / Mario M. Cuomo  Bridge',
+        'Triborough (Robert F. Kennedy Bridge)',
+        'Hugh L Carey (Brooklyn–Battery Tunnel)',
+        'Queens Midtown Tunnel',
+        'Verrazano-Narrows Bridge',
+        'Henry Hudson Bridge',
+        'Cross Bay Veterans Memorial Bridge',
+        'Marine Parkway-Gil Hodges Memorial Bridge',
+        'Atlantic Beach Toll Bridge',
+        'Ogdensburg-Prescott Bridge',
+        'Lewiston-Queenston Bridge',
+        'Whirlpool Rapids Bridge',
+        'Rainbow Bridge',
+        'Peace Bridge',
+        'Thousand Islands Bridge',
+        'Seaway International Bridge (Cornwall-Massena)',
+        'Whiteface Mountain Vet. Memorial Highway',
+        'Prospect Mountain Vet. Memorial Highway',
+        'Bear Mountain Bridge',
+        'Kingston-Rhinecliff Bridge',
+        'Mid-Hudson',
+        'Newburgh-Beacon Bridge',
+        'Rip Van Winkle Bridge',
+        'Bayonne Bridge',
+        'George Washington Bridge',
+        'Goethals Bridge',
+        'Outerbridge Crossing',
+        'Holland Tunnel',
+        'Lincoln Tunnel'
+    ]
+    end 
+
+    def get_user_bridges
+      @bridges.each_with_index {|month, idx| puts "#{idx + 1}. #{month}"}
     end
 end
--- lib/nyc_bridge_and_tunnel_fees/scraper.rb
+++ lib/nyc_bridge_and_tunnel_fees/scraper.rb
@@ -1,5 +1,17 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper 
 
-  
+  site = "https://tollguru.com/toll-info/new-york/toll-rates-for-new-york-bridges-and-tunnels"
+
+  doc = Nokogiri::HTML(open(site))
+  results = doc.css("div.page-content .table tbody tr")
+
+  results.each_with_index do |r, i|
+    # puts r.css("td.left").text.strip
+  end 
+
+#   puts results.count
 
 end 

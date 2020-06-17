f20c28f1c4ceab53334785c11bc85bbe55efe154
dcantoran <davidcantoran36@gmail.com>
2020-06-17 16:38:47 -0400

    successfully scraped

--- lib/nyc_bridge_and_tunnel_fees/bridge.rb
+++ lib/nyc_bridge_and_tunnel_fees/bridge.rb
@@ -1,11 +1,14 @@
 class NycBridgeAndTunnelFees::Bridge 
-  attr_accessor :name, :price
+  attr_accessor :name, :ezpass_price, :cash_price, :mail_price
   
   @@all = []
 
   def initialize
     @name = name
-    @price = price
+    @ezpass_price = ezpass_price
+    @cash_price = cash_price
+    @mail_price = mail_price
+
     save
   end 
   
@@ -24,14 +27,12 @@ class NycBridgeAndTunnelFees::Bridge
     bridge_info.each do |b| 
         bridge = NycBridgeAndTunnelFees::Bridge.new
         bridge.name = b.css("td.left").text.strip
-        # bridge.price = b.css("").text.strip
+        bridge.ezpass_price = b.css("td:nth-child(2)").text.strip
+        bridge.cash_price = b.css("td:nth-child(3)").text.strip
+        bridge.mail_price = b.css("td:nth-child(4)").text.strip
         binding.pry 
     end 
 
   end 
-  
-  def self.scrape_bridges
-    
-  end  
 
 end 
--- lib/nyc_bridge_and_tunnel_fees/scraper.rb
+++ lib/nyc_bridge_and_tunnel_fees/scraper.rb
@@ -5,13 +5,13 @@ class NycBridgeAndTunnelFees::Scraper
         doc = Nokogiri::HTML(open("https://tollguru.com/toll-info/new-york/toll-rates-for-new-york-bridges-and-tunnels"))
         # bridges = doc.css("div.page-content .table tbody tr")
         # bridges.each do |b|
-            # name = b.css("td.left").text.strip
-            # NycBridgeAndTunnelFees::Bridge.new(name)
+        #     name = b.css("td.left").text.strip
+        #     NycBridgeAndTunnelFees::Bridge.new(name)
         # end
     end 
 
 #   results.each_with_index do |r, i|
-    # puts r.css("td.left").text.strip
+#     puts r.css("td.left").text.strip
 #   end 
 
 #   puts results.count
